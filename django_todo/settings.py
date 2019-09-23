3d2f8eb61f98a3a0197b335a30b9ce5d41f746f1
Ubuntu <ubuntu@ip-172-31-95-205.ec2.internal>
2019-09-23 15:30:48 +0000

    attempting to fix a error that prevents heroku to display the site

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'py*al4se&q$+tl5jo+^8j!30#f=2f=u=lw%&@w6m8^0^*k#h0t'
 DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
-                os.environ.get('HOSTNAME')]
+                 os.environ.get('HOSTNAME')]
                 
 host = os.environ.get('SITE_HOST')
 if host:
